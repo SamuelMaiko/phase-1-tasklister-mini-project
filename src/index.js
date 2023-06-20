1c0ca0c9151720e00b36ebccfbbacbef117254ce
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-21 01:12:01 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form=document.getElementById('create-task-form');
+  form.addEventListener('submit',function(e){
+  e.preventDefault();
+  // console.log(e.target.nth-of-type);
+  const input=document.getElementById('new-task-description');
+  input.value
+  ///
+  const li=document.createElement("li");
+  
+      const ol=document.getElementById('tasks');
+      ol.appendChild(li);
+      li.textContent=`${input.value} `;
+      const deleteBtn=document.createElement("button");
+      ///
+  deleteBtn.addEventListener("click",function(e){
+  e.target.parentNode.remove();
+  })
+  //
+      li.appendChild(deleteBtn);
+      deleteBtn.innerHTML="x";
+      //
+  form.reset();
+  });
+  
+  
+  function deleteAllTasks(){
+  const removeAll=document.querySelector(".remove-all");
+  removeAll.addEventListener('click',function(){
+      const ol=document.getElementById('tasks');
+      ol.innerHTML='';
+  })}
+  
+  deleteAllTasks();
+  
 });
